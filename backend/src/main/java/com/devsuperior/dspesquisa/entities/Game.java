d9e92d7743042f2c772a1bde8266195f45fdb64b
Mateus Galdino <mateusgaldino1000@hotmail.com>
2020-09-14 17:30:02 -0300

    DTO, Service

--- backend/src/main/java/com/devsuperior/dspesquisa/entities/Game.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/entities/Game.java
@@ -18,6 +18,7 @@ import com.devsuperior.dspesquisa.entities.enums.Platform;
 @Entity
 @Table(name = "tb_game")
 public class Game implements Serializable{
+	
 	private static final long serialVersionUID = 1L;
 	
 	@Id
--- backend/src/main/java/com/devsuperior/dspesquisa/entities/Genre.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/entities/Genre.java
@@ -14,6 +14,7 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "tb_genre")
 public class Genre implements Serializable{
+	
 	private static final long serialVersionUID = 1L;
 	
 	@Id
